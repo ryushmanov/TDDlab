4f89b9b7f6f054d1485acaa183137b5a6cdf6818
Rodion Yushmanov <rodion.yushmanov@gmail.com>
2018-11-30 14:47:21 +0400

    Tests

--- App.cs
+++ App.cs
@@ -12,9 +12,40 @@ namespace TDDlab
 {
     public partial class App : Form
     {
+        public string[,] loginpass;
+        public string[] users;
+        public string path;
         public App()
         {
             InitializeComponent();
+        }
+
+        public void AddUser(object sender, EventArgs e)
+        {
+            passerr.Visible = false;
+            loginerr.Visible = false;
+            bool err = false;
+            err = EmptyCheck();
+            if (err == false)
+            {
+                for (int i = 0; i < loginpass.GetLength(0); i++)
+                {
+                    if (loginpass[i, 0].Equals(login.Text))
+                    {
+                        err = true;
+                        loginerr.Visible = true;
+                        toolTip.SetToolTip(loginerr, "Такой логин уже существует");
+                        break;
+                    }
+                }
+            }
+            if (err == false)
+            {
+                string s = login.Text + "\t" + PassHash(pass.Text);
+                AddLine(s);
+                MessageBox.Show("Пользователь успешно добавлен", "Добавление пользователя", MessageBoxButtons.OK);
+                GetUsers();
+            }
         }
     }
 }
